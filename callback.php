4cdd5c6c4f0c8f5c044845e192af5043dd0582f0
NaufalDhafin <naufal280905@gmail.com>
2024-03-31 04:25:24 +0700

    update

--- callback.php
+++
@@ -1,7 +0,0 @@
-<?php
-$privateKey = 'ySD4N-i9pJt-Kle7p-iAxAF-6lIUc';
-
-// ambil data json callback notifikasi
-$json = file_get_contents('php://input');
-$signature = hash_hmac('sha256', $json, $privateKey);
-?>
